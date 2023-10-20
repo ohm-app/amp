9d7ce03a0818f082247256abe45f898805fc2f8a
bitfl0wer <florian@pro-weber.com>
2023-10-20 20:43:23 +0200

    Add Bluetooth test to mod tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,44 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
+
+    use btleplug::api::{Central, Manager as _, ScanFilter};
+    use btleplug::platform::Manager;
+    use tokio::time;
+
     use super::*;
 
     #[test]
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[tokio::test]
+    async fn test_bluetooth() {
+        let manager = Manager::new().await.unwrap();
+        let adapter_list = manager.adapters().await.unwrap();
+        if adapter_list.is_empty() {
+            eprintln!("No Bluetooth adapters found");
+        }
+        for adapter in adapter_list.iter() {
+            println!(
+                "Starting scan on {}...",
+                adapter.adapter_info().await.unwrap()
+            );
+            adapter
+                .start_scan(ScanFilter::default())
+                .await
+                .expect("Can't scan BLE adapter for connected devices...");
+            time::sleep(Duration::from_secs(10)).await;
+            let peripherals = adapter.peripherals().await.unwrap();
+            if peripherals.is_empty() {
+                eprintln!("->>> BLE peripheral devices were not found, sorry. Exiting...");
+            } else {
+                println!("Success!");
+                dbg!(peripherals);
+            }
+        }
     }
 }
