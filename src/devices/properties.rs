bcbbf624ee1ed4bdde50d60fa5174c203a95ee61
bitfl0wer <florian@pro-weber.com>
2023-11-07 20:13:55 +0100

    Refactor power module and add enums for BatteryType and PowerState

--- src/devices/properties.rs
+++ src/devices/properties.rs
@@ -1,5 +1,5 @@
 pub mod power {
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, strum_macros::Display)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
     pub enum BatteryState {
         Charging(u8),
         Discharging(u8),
@@ -7,26 +7,34 @@ pub mod power {
         Unknown,
     }
 
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, strum_macros::Display)]
+    #[cfg_attr(feature = "strum_derive", derive(strum_macros::Display))]
+    #[cfg_attr(feature = "serde_derive", derive(serde::Serialize, serde::Deserialize))]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
     pub enum BatteryType {
         LeftRightCase(BatteryState, BatteryState, BatteryState),
         Single(BatteryState),
     }
 
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, strum_macros::Display)]
+    #[cfg_attr(feature = "strum_derive", derive(strum_macros::Display))]
+    #[cfg_attr(feature = "serde_derive", derive(serde::Serialize, serde::Deserialize))]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
     pub enum PowerState {
         On,
         Off,
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, strum_macros::Display)]
+#[cfg_attr(feature = "strum_derive", derive(strum_macros::Display))]
+#[cfg_attr(feature = "serde_derive", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum DeviceType {
     Headphones,
     Earbuds,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, strum_macros::Display)]
+#[cfg_attr(feature = "strum_derive", derive(strum_macros::Display))]
+#[cfg_attr(feature = "serde_derive", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum AmbientSound {
     NoiseCancelling(u8),
     Off,
